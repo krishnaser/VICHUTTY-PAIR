a15216ff5d163a5edbee2bcae70f51dc0f5c5c7b
KRISHNA <121825759+krishnaser@users.noreply.github.com>
2024-06-01 20:30:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://godzenitsu:godzenitsu@cluster0.czhj8cm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://krishnadied:yAc0S5qAkokNQKYl@cluster0.crzzpwi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   SESSION_NAME: process.env.SESSION_NAME || "VICHUTTY-MD~"
 };
